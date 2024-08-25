0ce9d8f58d22ab8924c76014c2be9ba10f9dc633
fausto perez <faustperez22@gmail.com>
2024-08-24 19:09:08 -0500

    Fin de seccion 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { CounterModule } from './counter/counter.module';
+import { HeroesModule } from './heroes/heroes.module';
+
+//import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    CounterModule,
+    HeroesModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/counter/components/counter.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  template: `
+  <h3>Counter: {{counter}}</h3>
+
+  <button (click)="increaseBy(+1)">+1</button>
+  <button (click)="resetBy()">Reset</button>
+  <button (click)="increaseBy(-1)">-1</button>
+
+  `
+})
+
+export class CounterComponent {
+  constructor() { }
+  public title: string = 'Hola Mundo';
+  public counter: number = 10;
+
+  increaseBy(value:number):void{
+    this.counter += value;
+  }
+
+  resetBy(){
+    this.counter = 10;
+  }
+
+}
